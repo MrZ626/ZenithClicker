9361b52f8e5148cb6a41625fccb612ebdbdb9693
MrZ_26 <1046101471@qq.com>
2025-12-24 19:10:39 +0800

    # Happy Christmas!

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011314,
-    ["verCode"] = 011314,
+    ["apkCode"] = 011315,
+    ["verCode"] = 011315,
 
-    ["appVer"] = "V1.13.14",
-    ["verStr"] = "1.13.14",
+    ["appVer"] = "V1.13.15",
+    ["verStr"] = "1.13.15",
 }
