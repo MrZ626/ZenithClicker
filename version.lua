311cb928f503a76f88ccde99f325880e4cf5ff65
MrZ_26 <1046101471@qq.com>
2025-12-27 22:30:39 +0800

    # Fix - Fix Christmas theme always activated - Rename an achivement

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011319,
-    ["verCode"] = 011319,
+    ["apkCode"] = 011320,
+    ["verCode"] = 011320,
 
-    ["appVer"] = "V1.13.19",
-    ["verStr"] = "1.13.19",
+    ["appVer"] = "V1.13.20",
+    ["verStr"] = "1.13.20",
 }
