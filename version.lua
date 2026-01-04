4c4fde07ef33353987a6810febf07d4ea3fbd9b9
MrZ_26 <1046101471@qq.com>
2026-01-04 22:33:12 +0800

    # Fix - IN's ZPmul improved to 1.2x from 1.1x - Fixed a small bug in record page

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011321,
-    ["verCode"] = 011321,
+    ["apkCode"] = 011322,
+    ["verCode"] = 011322,
 
-    ["appVer"] = "V1.13.21",
-    ["verStr"] = "1.13.21",
+    ["appVer"] = "V1.13.22",
+    ["verStr"] = "1.13.22",
 }
