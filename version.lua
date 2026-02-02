dc2469eb26844066718e5d12a807e943d4e5616c
MrZ_26 <1046101471@qq.com>
2026-02-02 10:45:25 +0800

    # Change - Now the rank-time tracker can track to R-62 at max (also affect Cruise Control) - Fixed the 2nd outro of F3 music not being used - Update community combo database - Fixed some internal issues

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011322,
-    ["verCode"] = 011322,
+    ["apkCode"] = 011323,
+    ["verCode"] = 011323,
 
-    ["appVer"] = "V1.13.22",
-    ["verStr"] = "1.13.22",
+    ["appVer"] = "V1.13.23",
+    ["verStr"] = "1.13.23",
 }
