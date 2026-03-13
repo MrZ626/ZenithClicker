4dacea8d16f88d7cdd9c8c355ad54a569bf609d2
trevor-smithy <trevor.smithy100@gmail.com>
2026-03-13 08:27:17 -0500

    Music Player Update
    Can now play the 3 custom TERASPEED variants with the music player in CONF, also added lyrics for the 2 variants they're relevant for.
    How to activate:
    TERA-E (i.e. Smithy Mode): CTRL+ALT+T OR paste `mp` or `music` while "The Prog(r)am(m)ing Smithy" is active
    TERA-L (i.e. Uneasy Mode):
    CTRL+T & Ultra OR paste `mp` or `music` while any Uneasy is active
    TERA-EL (i.e. Uneasy Smithy Mode):
    CTRL+ALT+T & Ultra OR paste `mp` or `music` while "The Uneasy Prog(r)am(m)ing Smithy" is active

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker: Easy Mode",
-    ["apkCode"] = 010003,
-    ["verCode"] = 010003,
+    ["apkCode"] = 010004,
+    ["verCode"] = 010004,
 
-    ["appVer"] = "eV1.00.03",
-    ["verStr"] = "1.00.03",
+    ["appVer"] = "eV1.00.04",
+    ["verStr"] = "1.00.04",
 }
