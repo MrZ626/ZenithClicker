9b024062e393ebff01201667033427641484d6a8
MrZ_26 <1046101471@qq.com>
2026-04-30 23:44:28 +0800

    # New - New true killscreen - ZP reworked - 2 new achievements

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011327,
-    ["verCode"] = 011327,
+    ["apkCode"] = 011328,
+    ["verCode"] = 011328,
 
-    ["appVer"] = "V1.13.27",
-    ["verStr"] = "1.13.27",
+    ["appVer"] = "V1.13.28",
+    ["verStr"] = "1.13.28",
 }
