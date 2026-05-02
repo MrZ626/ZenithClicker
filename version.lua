78dcbbb1c0697917122446dd8436cd93b8bfaf2e
MrZ_26 <1046101471@qq.com>
2026-05-03 01:49:35 +0800

    # New - Added Windup animation - Fixed stats not being fully saved when run finished - Fixed UI mistakes and typos

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011400,
-    ["verCode"] = 011400,
+    ["apkCode"] = 011401,
+    ["verCode"] = 011401,
 
-    ["appVer"] = "V1.14.0",
-    ["verStr"] = "1.14.0",
+    ["appVer"] = "V1.14.1",
+    ["verStr"] = "1.14.1",
 }
