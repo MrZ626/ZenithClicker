498a28d287fc6c2be6718096a5cd410dc136effb
MrZ_26 <1046101471@qq.com>
2026-06-06 06:12:27 +0800

    # New - Added speedrun milestone page - More safety checks in backup system - Fixed speedrun timer reset on each launch

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011602,
-    ["verCode"] = 011602,
+    ["apkCode"] = 011603,
+    ["verCode"] = 011603,
 
-    ["appVer"] = "V1.16.2",
-    ["verStr"] = "1.16.2",
+    ["appVer"] = "V1.16.3",
+    ["verStr"] = "1.16.3",
 }
