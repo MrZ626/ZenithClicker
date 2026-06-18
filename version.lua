fca523be64749d91d34b11e0d77d6c389a59c858
MrZ_26 <1046101471@qq.com>
2026-06-18 08:08:55 +0800

    # Fixed - Improved many details about daily challenge leaderboard - Improved spike counter a bit - Many technical optimizations

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011701,
-    ["verCode"] = 011701,
+    ["apkCode"] = 011702,
+    ["verCode"] = 011702,
 
-    ["appVer"] = "V1.17.1",
-    ["verStr"] = "1.17.1",
+    ["appVer"] = "V1.17.2",
+    ["verStr"] = "1.17.2",
 }
